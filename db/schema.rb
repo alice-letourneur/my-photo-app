a9e1c7033cbaa7f4019c210259b1e2bd042b363b
“Alice <aliceletourneur@icloud.com>
2018-11-18 17:39:26 +0000

    Create Payment model and update sign up form to accept payment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_195017) do
+ActiveRecord::Schema.define(version: 2018_11_18_163609) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
